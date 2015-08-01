d4cb41ef5972ec0136a6acf75271061c7abd1f35
Andrew Soltan <andrewsoltan@yahoo.com>
2015-08-01 12:03:19 -0700

    Initial Commit

---
+++ GradleTest/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  GradleTest
+//
+//  Created by Andrew Soltan on 8/1/15.
+//  Copyright (c) 2015 Andrew Soltan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
